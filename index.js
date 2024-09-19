a1bba513cfcee8d35e6dcb564b91390c2bf72080
mosesnzioka <mosesnzioka1@gmail.com>
2024-09-19 16:18:14 +0300

    adding user interface

---
+++ index.js
@@ -0,0 +1,6 @@
+const handrock=document.querySelector(".rock");
+handrock.addEventListener("click", function(){
+    //alert("button clicked");
+    document.querySelector('.player-choice-display').innerHTML="&#9994;"
+    // document.getElementsByClassName("rock").innerhtml = document.getElementsByClassName("player").innerhtml;
+})
